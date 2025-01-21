0777b85f3c480d976c558cee9262faa4b754fe0f
MatheusAvilla <matheusavillavial@gmail.com>
2025-01-20 23:37:17 -0300

    Deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  base: "/kaplay-runner/",
-  resolve: {
-    alias: {
-      kaplay: path.resolve(__dirname, 'node_modules/kaplay/dist/kaplay.js'),
-    },
-  },
-  optimizeDeps: {
-    include: ['kaplay']
-  }
+  base: "/kaplay-runner/"
 });
